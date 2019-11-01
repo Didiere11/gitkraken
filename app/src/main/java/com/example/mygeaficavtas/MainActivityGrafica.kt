7b7c89e4ec6b37023424266980888d4ef3165761
Didiere11 <ramirezrenteriajoseididere@gmail.com>
2019-11-01 15:16:51 -0600

    primer

---
+++ app/src/main/java/com/example/mygeaficavtas/MainActivityGrafica.kt
@@ -0,0 +1,50 @@
+package com.example.mygeaficavtas
+
+import android.database.Cursor
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import kotlinx.android.synthetic.main.activity_main_grafica.*
+
+class MainActivityGrafica : AppCompatActivity() {
+    val entries = ArrayList<BarEntry>()
+    val labels = ArrayList<String>()
+    var cursor : Cursor? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_grafica)
+        cargarDatos()
+        setBarChart()
+    }
+    fun cargarDatos(){
+        val admin = AdminBD(this)
+        cursor=admin.consulta("Select * from Empleado Order by Ventas")
+    }
+
+    fun setBarChart() {
+        //Agregamos datos de tipo float, en el primer paramtro
+        // se indica el angulo de las Y y en en
+        // segundo parametro el lado de X
+        var i = 0
+        if (cursor!!.moveToFirst()){
+            do {
+                val nom = cursor!!.getString(1)
+                val vtas = cursor!!.getFloat(2)
+                entries.add(BarEntry(vtas, i))
+                labels.add(nom)
+                i++
+            }while (cursor!!.moveToNext())
+            val barDataSet = BarDataSet(entries, "Datos")
+            val data = BarData(labels, barDataSet)
+            barChart.data = data
+            barChart.setDescription("Graficas Ejemplo")
+            barDataSet.color = resources.getColor(R.color.colorAccent)
+            barChart.animateY(5000)
+
+        }
+
+    }
+
+}
